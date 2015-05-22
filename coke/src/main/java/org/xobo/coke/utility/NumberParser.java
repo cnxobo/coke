2d7674e044d171e0cbbef5a039b1b8147ae73383
bing <i@xobo.org>
2015-05-22 09:54:36 +0800

    添加 解析 Integer

--- coke/src/main/java/org/xobo/coke/utility/NumberParser.java
+++ coke/src/main/java/org/xobo/coke/utility/NumberParser.java
@@ -1,5 +1,10 @@
 package org.xobo.coke.utility;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.util.StringUtils;
+
 public class NumberParser {
 	public static Long parseLong(Object obj) {
 		if (obj == null) {
@@ -16,5 +21,39 @@ public class NumberParser {
 			return val;
 		}
 
+	}
+
+	public static Integer parseInteger(Object obj) {
+		if (obj == null) {
+			return null;
+		} else if (obj instanceof Number) {
+			return ((Number) obj).intValue();
+		} else {
+			Integer val = null;
+			try {
+				val = Integer.parseInt(obj.toString());
+			} catch (Exception e) {
+
+			}
+			return val;
+		}
+	}
+
+	public static List<Integer> parseIntegerList(String str) {
+		return parseIntegerList(str, ",");
+	}
+
+	public static List<Integer> parseIntegerList(String str, String regex) {
+		List<Integer> list = new ArrayList<Integer>();
+		if (StringUtils.hasText(str)) {
+			String[] tokens = str.split(regex);
+			for (String token : tokens) {
+				Integer val = parseInteger(token);
+				if (val != null) {
+					list.add(val);
+				}
+			}
+		}
+		return list;
 	}
 }
