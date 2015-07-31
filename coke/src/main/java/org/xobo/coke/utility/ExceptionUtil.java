d8af7c40949cc3411297803e9287607048479e3b
bing <i@xobo.org>
2015-07-31 13:55:37 +0800

    修改方法名

--- coke/src/main/java/org/xobo/coke/utility/ExceptionUtil.java
+++ coke/src/main/java/org/xobo/coke/utility/ExceptionUtil.java
@@ -4,7 +4,7 @@ import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
 
 public class ExceptionUtil {
-	public static String exceptionStacktraceToString(Exception e) {
+	public static String stackTraceToString(Throwable e) {
 		ByteArrayOutputStream baos = new ByteArrayOutputStream();
 		PrintStream ps = new PrintStream(baos);
 		e.printStackTrace(ps);
