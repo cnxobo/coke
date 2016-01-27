c97e0d4e5d32d6d1b32946971350ad3bf2cf6def
Bing <i@xobo.org>
2016-01-27 18:08:38 +0800

    字条串，默认使用 xx%

--- coke/src/main/java/org/xobo/coke/querysupporter/service/impl/SqlBuilderImpl.java
+++ coke/src/main/java/org/xobo/coke/querysupporter/service/impl/SqlBuilderImpl.java
@@ -23,157 +23,161 @@ import com.bstek.dorado.data.provider.filter.SingleValueFilterCriterion;
 @Service(SqlBuilder.BEAN_ID)
 public class SqlBuilderImpl implements SqlBuilder {
 
-	@Resource(name = DoradoCriteriaBuilderImpl.BEAN_ID)
-	private DoradoCriteriaBuilderImpl doradoCriteriaBuilder;
+  @Resource(name = DoradoCriteriaBuilderImpl.BEAN_ID)
+  private DoradoCriteriaBuilderImpl doradoCriteriaBuilder;
 
-	@Resource(name = SynonymService.BEAN_ID)
-	private SynonymService synonymService;
+  @Resource(name = SynonymService.BEAN_ID)
+  private SynonymService synonymService;
 
-	@Override
-	public QueryResolver extractCriteria(Class<?> clazz, Criteria criteria, String alias) {
-		int parameterNameCount = 0;
-		if (criteria == null) {
-			return null;
-		}
-		QueryResolver result = new QueryResolver();
+  @Override
+  public QueryResolver extractCriteria(Class<?> clazz, Criteria criteria, String alias) {
+    int parameterNameCount = 0;
+    if (criteria == null) {
+      return null;
+    }
+    QueryResolver result = new QueryResolver();
 
-		// build where
-		if (!criteria.getCriterions().isEmpty()) {
-			StringBuilder whereCondition = result.getWhere();
-			Map<String, Object> valueMap = result.getValueMap();
-			int count = 0;
-			for (Criterion c : criteria.getCriterions()) {
-				if (count > 0) {
-					whereCondition.append(" and ");
-				}
-				count++;
-				parameterNameCount = buildCriterion(clazz, whereCondition, c, valueMap, parameterNameCount, alias);
-			}
-		}
+    // build where
+    if (!criteria.getCriterions().isEmpty()) {
+      StringBuilder whereCondition = result.getWhere();
+      Map<String, Object> valueMap = result.getValueMap();
+      int count = 0;
+      for (Criterion c : criteria.getCriterions()) {
+        if (count > 0) {
+          whereCondition.append(" and ");
+        }
+        count++;
+        parameterNameCount =
+            buildCriterion(clazz, whereCondition, c, valueMap, parameterNameCount, alias);
+      }
+    }
 
-		// build order
-		StringBuilder orderBuilder = result.getOrder();
-		List<Order> orders = criteria.getOrders();
-		for (Order order : orders) {
-			if (orderBuilder.length() > 0) {
-				orderBuilder.append(",");
-			}
+    // build order
+    StringBuilder orderBuilder = result.getOrder();
+    List<Order> orders = criteria.getOrders();
+    for (Order order : orders) {
+      if (orderBuilder.length() > 0) {
+        orderBuilder.append(",");
+      }
 
-			if (StringUtils.isNotEmpty(alias)) {
-				orderBuilder.append(" " + alias + "." + order.getProperty());
-			} else {
-				orderBuilder.append(" " + order.getProperty());
-			}
+      if (StringUtils.isNotEmpty(alias)) {
+        orderBuilder.append(" " + alias + "." + order.getProperty());
+      } else {
+        orderBuilder.append(" " + order.getProperty());
+      }
 
-			if (order.isDesc()) {
-				orderBuilder.append(" desc");
-			} else {
-				orderBuilder.append(" asc");
-			}
-		}
-		return result;
-	}
+      if (order.isDesc()) {
+        orderBuilder.append(" desc");
+      } else {
+        orderBuilder.append(" asc");
+      }
+    }
+    return result;
+  }
 
-	@Override
-	public QueryResolver extractQuery(Class<?> clazz, Criteria criteria, Map<String, Object> queryParameter,
-			String alias) {
-		criteria = doradoCriteriaBuilder.mergeQueryParameterCriteria(queryParameter, null, criteria, clazz);
-		return extractCriteria(clazz, criteria, alias);
-	}
+  @Override
+  public QueryResolver extractQuery(Class<?> clazz, Criteria criteria,
+      Map<String, Object> queryParameter, String alias) {
+    criteria =
+        doradoCriteriaBuilder.mergeQueryParameterCriteria(queryParameter, null, criteria, clazz);
+    return extractCriteria(clazz, criteria, alias);
+  }
 
-	private int buildCriterion(Class<?> clazz, StringBuilder sb, Criterion c, Map<String, Object> valueMap,
-			int parameterNameCount, String alias) {
-		if (c instanceof SingleValueFilterCriterion) {
-			parameterNameCount++;
-			SingleValueFilterCriterion fc = (SingleValueFilterCriterion) c;
-			String operator = buildOperator(fc.getFilterOperator());
-			String propertyName = buildFieldName(fc.getProperty());
+  private int buildCriterion(Class<?> clazz, StringBuilder sb, Criterion c,
+      Map<String, Object> valueMap, int parameterNameCount, String alias) {
+    if (c instanceof SingleValueFilterCriterion) {
+      parameterNameCount++;
+      SingleValueFilterCriterion fc = (SingleValueFilterCriterion) c;
+      String operator = buildOperator(fc.getFilterOperator());
+      String propertyName = buildFieldName(fc.getProperty());
 
-			Collection<String> unionPropertyList = synonymService.find(clazz, propertyName);
+      Collection<String> unionPropertyList = synonymService.find(clazz, propertyName);
 
-			if (unionPropertyList.isEmpty()) {
-				singleValueFilterCriterion(sb, valueMap, parameterNameCount, alias, propertyName, operator,
-						fc.getValue());
-			} else {
-				sb.append(" ( ");
-				boolean first = true;
-				for (String property : unionPropertyList) {
-					if (!first) {
-						sb.append(" or ");
-					} else {
-						first = false;
-					}
-					singleValueFilterCriterion(sb, valueMap, parameterNameCount, alias, property, operator,
-							fc.getValue());
-				}
-				sb.append(" ) ");
-			}
-		}
-		if (c instanceof Junction) {
-			Junction jun = (Junction) c;
-			String junction = " and ";
-			if (jun instanceof Or) {
-				junction = " or ";
-			}
-			int count = 0;
-			Collection<Criterion> criterions = jun.getCriterions();
-			if (criterions != null) {
-				sb.append(" ( ");
-				for (Criterion criterion : criterions) {
-					if (count > 0) {
-						sb.append(junction);
-					}
-					count++;
-					parameterNameCount = buildCriterion(clazz, sb, criterion, valueMap, parameterNameCount, alias);
-				}
-				sb.append(" ) ");
-			}
-		}
-		return parameterNameCount;
-	}
+      if (unionPropertyList.isEmpty()) {
+        singleValueFilterCriterion(sb, valueMap, parameterNameCount, alias, propertyName, operator,
+            fc.getValue());
+      } else {
+        sb.append(" ( ");
+        boolean first = true;
+        for (String property : unionPropertyList) {
+          if (!first) {
+            sb.append(" or ");
+          } else {
+            first = false;
+          }
+          singleValueFilterCriterion(sb, valueMap, parameterNameCount, alias, property, operator,
+              fc.getValue());
+        }
+        sb.append(" ) ");
+      }
+    }
+    if (c instanceof Junction) {
+      Junction jun = (Junction) c;
+      String junction = " and ";
+      if (jun instanceof Or) {
+        junction = " or ";
+      }
+      int count = 0;
+      Collection<Criterion> criterions = jun.getCriterions();
+      if (criterions != null) {
+        sb.append(" ( ");
+        for (Criterion criterion : criterions) {
+          if (count > 0) {
+            sb.append(junction);
+          }
+          count++;
+          parameterNameCount =
+              buildCriterion(clazz, sb, criterion, valueMap, parameterNameCount, alias);
+        }
+        sb.append(" ) ");
+      }
+    }
+    return parameterNameCount;
+  }
 
-	private String processLike(String operator) {
-		String result = operator;
-		if (operator.endsWith("*")) {
-			result = operator.substring(0, operator.length() - 1);
-		}
-		if (operator.startsWith("*")) {
-			result = operator.substring(1, operator.length());
-		}
-		return result;
-	}
+  private String processLike(String operator) {
+    String result = operator;
+    if (operator.endsWith("*")) {
+      result = operator.substring(0, operator.length() - 1);
+    }
+    if (operator.startsWith("*")) {
+      result = operator.substring(1, operator.length());
+    }
+    return result;
+  }
 
-	protected String buildFieldName(String name) {
-		return name;
-	}
+  protected String buildFieldName(String name) {
+    return name;
+  }
 
-	protected String buildOperator(FilterOperator filterOperator) {
-		String operator = "like";
-		if (filterOperator != null) {
-			operator = filterOperator.toString();
-		}
-		return operator;
-	}
+  protected String buildOperator(FilterOperator filterOperator) {
+    String operator = "like";
+    if (filterOperator != null) {
+      operator = filterOperator.toString();
+    }
+    return operator;
+  }
 
-	void singleValueFilterCriterion(StringBuilder sb, Map<String, Object> valueMap, int parameterNameCount,
-			String alias, String propertyName, String operator, Object value) {
-		if (StringUtils.isNotEmpty(alias)) {
-			sb.append(" " + alias + "." + propertyName);
-		} else {
-			sb.append(" " + propertyName);
-		}
-		sb.append(" " + processLike(operator) + " ");
-		String prepareName = propertyName + "_" + parameterNameCount + "_";
-		sb.append(" :" + prepareName + " ");
-		if (operator.equals("like")) {
-			valueMap.put(prepareName, "%" + value + "%");
-		} else if (operator.startsWith("*")) {
-			valueMap.put(prepareName, "%" + value);
-		} else if (operator.endsWith("*")) {
-			valueMap.put(prepareName, value + "%");
-		} else {
-			valueMap.put(prepareName, value);
-		}
-	}
+  void singleValueFilterCriterion(StringBuilder sb, Map<String, Object> valueMap,
+      int parameterNameCount, String alias, String propertyName, String operator, Object value) {
+    if (StringUtils.isNotEmpty(alias)) {
+      sb.append(" " + alias + "." + propertyName);
+    } else {
+      sb.append(" " + propertyName);
+    }
+    sb.append(" " + processLike(operator) + " ");
+    String prepareName = propertyName + "_" + parameterNameCount + "_";
+    sb.append(" :" + prepareName + " ");
+
+    if (value instanceof String) {
+      if (operator.equals("like")) {
+        value = value + "%";
+      } else if (operator.startsWith("*")) {
+        value = "%" + value;
+      }
+      value = ((String) value).replaceAll("\\*", "%").replaceAll("\\?", "_");
+    }
+    valueMap.put(prepareName, value);
+  }
 
 }
