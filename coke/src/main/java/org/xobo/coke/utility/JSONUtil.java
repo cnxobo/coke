e8a0a8d6364078a9adfe37f98af647e39c3b84e6
Bing <i@xobo.org>
2016-12-09 15:59:43 +0800

    添加toXML method

--- coke/src/main/java/org/xobo/coke/utility/JSONUtil.java
+++ coke/src/main/java/org/xobo/coke/utility/JSONUtil.java
@@ -8,6 +8,7 @@ import java.util.List;
 import java.util.Map;
 
 import com.fasterxml.jackson.annotation.JsonFilter;
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.core.TreeNode;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.JavaType;
@@ -16,32 +17,12 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.ser.FilterProvider;
 import com.fasterxml.jackson.databind.ser.impl.SimpleBeanPropertyFilter;
 import com.fasterxml.jackson.databind.ser.impl.SimpleFilterProvider;
+import com.fasterxml.jackson.dataformat.xml.JacksonXmlModule;
+import com.fasterxml.jackson.dataformat.xml.XmlMapper;
 
 public class JSONUtil {
-  public static final String IGNORE_PROPERTEIS_FILTER = "ignoreProperteisFilter";
   public static final String DEFAULT_DATE_FORMATE = "yyyy-MM-dd HH:mm:ss";
-
-  /**
-   * 对象转JSON字符串
-   * 
-   * @param object
-   * @return
-   */
-  public static String toJSON(Object object) {
-    return toJSON(object, DEFAULT_DATE_FORMATE);
-  }
-
-  public static String toJSON(Object object, String dateFormat) {
-
-    ObjectMapper mapper = buildObjectMapper(dateFormat);
-    StringWriter writer = new StringWriter();
-    try {
-      mapper.writeValue(writer, object);
-    } catch (Exception e) {
-      throw new RuntimeException(e);
-    }
-    return writer.toString();
-  }
+  public static final String IGNORE_PROPERTEIS_FILTER = "ignoreProperteisFilter";
 
   /**
    * 获取ObjectMapper
@@ -63,6 +44,64 @@ public class JSONUtil {
     return mapper;
   }
 
+  /**
+   * JSON字符串转集合
+   * 
+   * @param json
+   * @param clazz
+   * @return
+   */
+  public static <T> List<T> jsonToList(String json, Class<T> clazz) {
+    ObjectMapper mapper = buildObjectMapper();
+    JavaType javaType =
+        mapper.getTypeFactory().constructCollectionType(List.class, clazz);
+    try {
+      return mapper.readValue(json, javaType);
+    } catch (IOException e) {
+      throw new RuntimeException(e);
+    }
+  }
+
+  /**
+   * 对象转JSON字符串并格式化输出.
+   * 
+   * @param object
+   * @return
+   */
+  public static String prettyJSON(Object object) {
+    String json = null;
+    ObjectMapper mapper = buildObjectMapper();
+    try {
+      json = mapper.writerWithDefaultPrettyPrinter().writeValueAsString(object);
+    } catch (Exception e) {
+      throw new RuntimeException(e);
+    }
+    return json;
+  }
+
+
+  /**
+   * 对象转JSON字符串
+   * 
+   * @param object
+   * @return
+   */
+  public static String toJSON(Object object) {
+    return toJSON(object, DEFAULT_DATE_FORMATE);
+  }
+
+
+  public static String toJSON(Object object, String dateFormat) {
+
+    ObjectMapper mapper = buildObjectMapper(dateFormat);
+    StringWriter writer = new StringWriter();
+    try {
+      mapper.writeValue(writer, object);
+    } catch (Exception e) {
+      throw new RuntimeException(e);
+    }
+    return writer.toString();
+  }
 
   /**
    * 对象转JSON字符串, 并忽略指定属性.
@@ -85,6 +124,96 @@ public class JSONUtil {
     return writer.toString();
   }
 
+  public static String toXML(Object object) {
+    XmlMapper xmlMapper = new XmlMapper();
+    try {
+      return xmlMapper.writeValueAsString(object);
+    } catch (JsonProcessingException e) {
+      return null;
+    }
+  }
+
+  /**
+   * 对象解析为JSON Node
+   * 
+   * @param obj
+   * @return
+   */
+  public static JsonNode toJsonNode(Object obj) {
+    if (obj == null) {
+      return null;
+    }
+    ObjectMapper mapper = buildObjectMapper();
+    return mapper.valueToTree(obj);
+  }
+
+  /**
+   * @param json
+   * @return
+   */
+  public static JsonNode toJsonNode(String json) {
+    if (json == null) {
+      return null;
+    }
+    ObjectMapper mapper = buildObjectMapper();
+    try {
+      return mapper.readTree(json);
+    } catch (Exception e) {
+      throw new RuntimeException(e);
+    }
+  }
+
+  public static <T> List<T> toList(String json, Class<T> clazz) {
+    if (json == null) {
+      return null;
+    }
+    ObjectMapper mapper = buildObjectMapper();
+    try {
+      JavaType javaType = mapper.getTypeFactory()
+          .constructCollectionType(List.class, clazz);
+      return mapper.readValue(json, javaType);
+    } catch (Exception e) {
+      throw new RuntimeException(e);
+    }
+  }
+
+  /**
+   * Java对象转Map
+   * 
+   * @param obj
+   * @return
+   */
+  public static Map<String, Object> toMap(Object obj) {
+    ObjectMapper mapper = buildObjectMapper();
+    JavaType javaType =
+        mapper.getTypeFactory().constructParametrizedType(LinkedHashMap.class, Map.class,
+            String.class, Object.class);
+    Map<String, Object> props = mapper.convertValue(obj, javaType);
+    return props;
+  }
+
+
+
+  /**
+   * JSON字符串转Map
+   * 
+   * @param json
+   * @return
+   */
+  public static Map<String, Object> toMap(String json) {
+    if (json == null) {
+      return null;
+    }
+    ObjectMapper mapper = buildObjectMapper();
+    try {
+      JavaType javaType =
+          mapper.getTypeFactory().constructParametrizedType(LinkedHashMap.class, Map.class,
+              String.class, Object.class);
+      return mapper.readValue(json, javaType);
+    } catch (Exception e) {
+      throw new RuntimeException(e);
+    }
+  }
 
   /**
    * JSON字符串转对象
@@ -138,123 +267,21 @@ public class JSONUtil {
     }
   }
 
-  /**
-   * 对象解析为JSON Node
-   * 
-   * @param obj
-   * @return
-   */
-  public static JsonNode toJsonNode(Object obj) {
-    if (obj == null) {
+  @SuppressWarnings("unchecked")
+  public static Map<String, Object> xmlToMap(String xml) {
+    if (xml == null) {
       return null;
     }
-    ObjectMapper mapper = buildObjectMapper();
-    return mapper.valueToTree(obj);
-  }
-
-  /**
-   * @param json
-   * @return
-   */
-  public static JsonNode toJsonNode(String json) {
-    if (json == null) {
-      return null;
-    }
-    ObjectMapper mapper = buildObjectMapper();
+    JacksonXmlModule module = new JacksonXmlModule();
+    XmlMapper xmlMapper = new XmlMapper(module);
+    Map<String, Object> content = null;
     try {
-      return mapper.readTree(json);
-    } catch (Exception e) {
-      throw new RuntimeException(e);
-    }
-  }
-
-
-
-  /**
-   * JSON字符串转Map
-   * 
-   * @param json
-   * @return
-   */
-  public static Map<String, Object> toMap(String json) {
-    if (json == null) {
-      return null;
-    }
-    ObjectMapper mapper = buildObjectMapper();
-    try {
-      JavaType javaType =
-          mapper.getTypeFactory().constructParametrizedType(LinkedHashMap.class, Map.class,
-              String.class, Object.class);
-      return mapper.readValue(json, javaType);
-    } catch (Exception e) {
-      throw new RuntimeException(e);
-    }
-  }
-
-  public static <T> List<T> toList(String json, Class<T> clazz) {
-    if (json == null) {
-      return null;
-    }
-    ObjectMapper mapper = buildObjectMapper();
-    try {
-      JavaType javaType = mapper.getTypeFactory()
-          .constructCollectionType(List.class, clazz);
-      return mapper.readValue(json, javaType);
-    } catch (Exception e) {
-      throw new RuntimeException(e);
-    }
-  }
-
-  /**
-   * Java对象转Map
-   * 
-   * @param obj
-   * @return
-   */
-  public static Map<String, Object> toMap(Object obj) {
-    ObjectMapper mapper = buildObjectMapper();
-    JavaType javaType =
-        mapper.getTypeFactory().constructParametrizedType(LinkedHashMap.class, Map.class,
-            String.class, Object.class);
-    Map<String, Object> props = mapper.convertValue(obj, javaType);
-    return props;
-  }
-
-  /**
-   * JSON字符串转集合
-   * 
-   * @param json
-   * @param clazz
-   * @return
-   */
-  public static <T> List<T> jsonToList(String json, Class<T> clazz) {
-    ObjectMapper mapper = buildObjectMapper();
-    JavaType javaType =
-        mapper.getTypeFactory().constructCollectionType(List.class, clazz);
-    try {
-      return mapper.readValue(json, javaType);
+      content = xmlMapper.readValue(xml, Map.class);
     } catch (IOException e) {
-      throw new RuntimeException(e);
+      e.printStackTrace();
     }
+    return content;
   }
-
-  /**
-   * 对象转JSON字符串并格式化输出.
-   * 
-   * @param object
-   * @return
-   */
-  public static String prettyJSON(Object object) {
-    String json = null;
-    ObjectMapper mapper = buildObjectMapper();
-    try {
-      json = mapper.writerWithDefaultPrettyPrinter().writeValueAsString(object);
-    } catch (Exception e) {
-      throw new RuntimeException(e);
-    }
-    return json;
-  }
-
 }
 
 
